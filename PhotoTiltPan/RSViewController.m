a77cf28127eb75f2d50b020b081b6ffbe81d183a
Ramsundar Shandilya <ram.shandilya@ymedialabs.com>
2014-07-16 09:55:29 +0530

    Basic project setup

--- PhotoTiltPan/RSViewController.m
+++ PhotoTiltPan/RSViewController.m
@@ -7,8 +7,14 @@
 //
 
 #import "RSViewController.h"
+#import <CoreMotion/CoreMotion.h>
 
 @interface RSViewController ()
+
+@property (weak, nonatomic) IBOutlet UIScrollView *photoScrollView;
+@property (weak, nonatomic) IBOutlet UIImageView *photoImageView;
+
+@property (nonatomic, strong) CMMotionManager *motionManager;
 
 @end
 
@@ -18,12 +24,33 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    self.motionManager = [[CMMotionManager alloc] init];
+}
+
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    
+    [self startMotionDetection];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)startMotionDetection
+{
+    [self.motionManager startDeviceMotionUpdatesUsingReferenceFrame:self.motionManager.attitudeReferenceFrame toQueue:[NSOperationQueue mainQueue] withHandler:^(CMDeviceMotion *motion, NSError *error) {
+        
+        CGFloat xRotationRate = motion.rotationRate.x;
+        CGFloat yRotationRate = motion.rotationRate.y;
+        CGFloat zRotationRate = motion.rotationRate.z;
+        
+    }];
 }
 
 @end
